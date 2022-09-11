3df7cd318b7040b78400621ab0750fa5049fb2ab
Gabriel1490 <martin>
2022-09-11 13:43:39 -0300

    Arreglos

--- administrador/config/bd.php
+++ administrador/config/bd.php
@@ -9,10 +9,5 @@ $conexion  = new mysqli($host, $usuario, $contraseña, $bd);
 if ($conexion->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
 }
-// try{
-// $conexion= new PDO("mysql:host=$host;dbname=$bd", $usuario,$contraseña);
-// if($conexion){echo "Conectado a sistema";}
-// } catch(Exception $ex){
-//     echo $ex->getMessage();
-// }
+
 ?>
